d4fe4045805a058dac7e1fa9f2806b3233c819bf
David Kent <phduong@gmail.com>
2019-10-17 10:03:43 +0700

    remove test.java

--- src/main/java/com/kms/gdrive/sheet/test.java
+++
@@ -1,9 +0,0 @@
-package com.kms.gdrive.sheet;
-
-public class test {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-    Sheet.setCredentialDir("gconf", null);
-    Report.updateTestResultByName("myTestcase", "Hello 123456", "TestResult", "1eaAz6HwGiZxJjpTIgasOzVegRFK2UnDUC2H6r0-SI2Q", false);
-  }
-}
